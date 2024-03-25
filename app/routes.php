efa90138ae88509b45f8b6c208aa65c317d9fcd7
Antonio <76910199+Manzano17@users.noreply.github.com>
2024-03-25 13:19:45 -0600

    Delete app directory

--- app/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Slim\App;
-
-return function (App $app) {
-    $app -> get('/', 'App\Controllers\HomeController:index');
-    $app -> get('/home', 'App\Controllers\HomeController:index');
-};
